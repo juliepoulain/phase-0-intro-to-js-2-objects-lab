c42c5bb28f22e14dda487e4c4ae14db779f53a3b
Julie Poulain <juliempoulain@gmail.com>
2024-02-26 18:07:57 -0700

    added functions for lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Julie Poulain",
+    streetAddress: "101 Dalmation Lane",
+  };
+
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee };
+    newEmployee[key] = value;
+    return newEmployee;
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee2 = { ...employee };
+    delete newEmployee2[key];
+    return newEmployee2;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
